f0593ee96e5b0473f322eb0091172668c1f4ac26
Dexter Fryar <dexter.fryar@outlook.com>
2015-04-01 13:51:58 -0500

    Added a filter for restricting the StatsD metrics
    A regular expression filter that allows the restriction
    of metrics that are sent by Storm / Threshold Engine to
    the Monasca StatsD agent/daemon.
    
    =======
    
    /etc/monasca/thresh-config.yml
    
    ```
    statsdConfig:
      host: localhost
      port: 8125
      prefix: monasca.storm.
      dimensions: !!map
        service : monitoring
        component : storm
      filter: ''
    ```
    
    host: IP or host where the Monasca Agent running a StatsD is running that will consume
          the metrics produced by Storm / Threshold Engine
    
    port: UDP port number where the Monasca Agent running a StatsD daemon that will consume
          the metrics produced by Storm / Threshold Engine
    
    prefix: This string will be prefixed to the internal metric
    
    dimensions: A map of key/value pairs that will be passed along as dimensions for each metric
    
    filter: A regular expression that filters out how much internal Storm / Threshold Engine metrics
            will be sent.  There is a default expression configured that allows the monitoring of
            Monasca/monitoring (MoM) to display the health in the Horizon dashboard (Monasca Health)
            available from the overview panel.
            If you want everything specify: '.*'
            If you want nothing specify ''
            If it doesn't exist you will get everything.
            Note: The default configuration produces ~40 metrics per minute and this can/will change
                  if the topology changes.
    
    Change-Id: Ia98597457a4b9b3b2cb61522049be9b4c51dddfc

--- thresh/src/main/java/monasca/thresh/utils/StatsdConfig.java
+++ thresh/src/main/java/monasca/thresh/utils/StatsdConfig.java
@@ -36,6 +36,16 @@ public class StatsdConfig implements Serializable {
 
   private Map<String, String> dimensions;
 
+  private String filter;
+
+  public String getFilter() {
+    return filter;
+  }
+
+  public void setFilter(String filter) {
+    this.filter = filter;
+  }
+
   public Map<String, String> getDimensions() {
     return dimensions;
   }
--- thresh/src/test/java/monasca/thresh/infrastructure/thresholding/AlarmThresholdingBoltTest.java
+++ thresh/src/test/java/monasca/thresh/infrastructure/thresholding/AlarmThresholdingBoltTest.java
@@ -64,7 +64,6 @@ public class AlarmThresholdingBoltTest {
   private AlarmDefinition alarmDefinition;
   private Alarm alarm;
   private List<SubAlarm> subAlarms;
-  private String subAlarmJson;
   private AlarmEventForwarder alarmEventForwarder;
   private AlarmDAO alarmDAO;
   private AlarmDefinitionDAO alarmDefinitionDAO;
@@ -126,7 +125,7 @@ public class AlarmThresholdingBoltTest {
             + "\"stateChangeReason\":\"Thresholds were exceeded for the sub-alarms: "
             + subAlarm.getExpression().getExpression() + " with the values: []\"," + "\"severity\":\"LOW\","
             + "\"subAlarms\":[" + buildSubAlarmJson(alarm.getSubAlarms()) + "],"
-            + "\"timestamp\":1395587091}}";
+            + "\"timestamp\":1395587091003}}";
 
     verify(alarmEventForwarder, times(1)).send(alarmJson);
     verify(alarmDAO, times(1)).updateState(alarmId, AlarmState.ALARM);
@@ -151,7 +150,7 @@ public class AlarmThresholdingBoltTest {
             + subAlarms.get(2).getExpression().getExpression() + " with the values: []"
             + "\",\"severity\":\"LOW\","
             + "\"subAlarms\":[" + buildSubAlarmJson(alarm.getSubAlarms()) + "],"
-            + "\"timestamp\":1395587091}}";
+            + "\"timestamp\":1395587091003}}";
     verify(alarmEventForwarder, times(1)).send(okJson);
     verify(alarmDAO, times(1)).updateState(alarmId, AlarmState.OK);
   }
@@ -328,7 +327,7 @@ public class AlarmThresholdingBoltTest {
     @Override
     protected long getTimestamp() {
       // Have to keep the time stamp constant so JSON comparison works
-      return 1395587091;
+      return 1395587091003l;
     }
   }
 }
