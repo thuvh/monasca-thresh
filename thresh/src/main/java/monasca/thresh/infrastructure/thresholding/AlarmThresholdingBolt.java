5355c746772ce0f54c125fb137475b52999b8b9c
Ryan Brandt <ryan.brandt@hp.com>
2015-04-30 12:02:00 -0600

    Clean up orphaned alarms on alarm definition delete
    Change-Id: I8dea800768989b80f1fe810f1c9572ed439d0133

--- thresh/src/main/java/monasca/thresh/infrastructure/thresholding/AlarmThresholdingBolt.java
+++ thresh/src/main/java/monasca/thresh/infrastructure/thresholding/AlarmThresholdingBolt.java
@@ -217,8 +217,16 @@ public class AlarmThresholdingBolt extends BaseRichBolt {
   }
 
   private void changeAlarmState(Alarm alarm, AlarmState initialState, String stateChangeReason) {
-    alarmDAO.updateState(alarm.getId(), alarm.getState());
     final AlarmDefinition alarmDefinition = alarmDefinitions.get(alarm.getAlarmDefinitionId());
+    // If the Alarm Definition id does not exist, ignore updating this alarm
+    if(alarmDefinition == null) {
+      logger.warn("Failed to locate alarm definition for id {},"
+                  + " ignoring state update to alarm with id {}",
+                  alarm.getAlarmDefinitionId(),
+                  alarm.getId());
+      return;
+    }
+    alarmDAO.updateState(alarm.getId(), alarm.getState());
     final List<MetricDefinition> alarmedMetrics = new ArrayList<>(alarm.getAlarmedMetrics().size());
     for (final MetricDefinitionAndTenantId mdtid : alarm.getAlarmedMetrics()) {
       alarmedMetrics.add(mdtid.metricDefinition);
