209a01c96db9ae6855bab90e152a8318efa455da
Dexter Fryar <dexter.fryar@outlook.com>
2015-07-27 18:58:29 -0500

    Added a whitelist for restricting the StatsD metrics
    A whitelist and metric map for the metrics that are
    sent by Storm / Threshold Engine to the Monasca
    StatsD agent/daemon.
    
    =======
    
    /etc/monasca/thresh-config.yml
    
    ```
    statsdConfig:
      host: localhost
      port: 8125
      debugmetrics: true
      dimensions: !!map
        service : monitoring
        component : storm
      whitelist: !!seq
        - aggregation-bolt.execute-count.filtering-bolt_alarm-creation-stream
        - aggregation-bolt.execute-count.filtering-bolt_default
        - aggregation-bolt.execute-count.system_tick
        - filtering-bolt.execute-count.event-bolt_metric-alarm-events
        - filtering-bolt.execute-count.metrics-spout_default
        - thresholding-bolt.execute-count.aggregation-bolt_default
        - thresholding-bolt.execute-count.event-bolt_alarm-definition-events
        - system.memory_heap.committedBytes
        - system.memory_nonHeap.committedBytes
        - system.newWorkerEvent
        - system.startTimeSecs
        - system.GC_ConcurrentMarkSweep.timeMs
      metricmap: !!map
        aggregation-bolt.execute-count.filtering-bolt_alarm-creation-stream :
          monasca.storm.aggregation-bolt.execute-count.filtering-bolt_alarm-creation-stream
        aggregation-bolt.execute-count.filtering-bolt_default :
          monasca.storm.aggregation-bolt.execute-count.filtering-bolt_default
        aggregation-bolt.execute-count.system_tick :
          monasca.storm.aggregation-bolt.execute-count.system_tick
        filtering-bolt.execute-count.event-bolt_metric-alarm-events :
          monasca.storm.filtering-bolt.execute-count.event-bolt_metric-alarm-events
        filtering-bolt.execute-count.metrics-spout_default :
          monasca.storm.filtering-bolt.execute-count.metrics-spout_default
        thresholding-bolt.execute-count.aggregation-bolt_default :
          monasca.storm.thresholding-bolt.execute-count.aggregation-bolt_default
        thresholding-bolt.execute-count.event-bolt_alarm-definition-events :
          monasca.storm.thresholding-bolt.execute-count.event-bolt_alarm-definition-events
        system.memory_heap.committedBytes :
          monasca.storm.system.memory_heap.committedBytes
        system.memory_nonHeap.committedBytes :
          monasca.storm.system.memory_nonHeap.committedBytes
        system.newWorkerEvent :
          monasca.storm.system.newWorkerEvent
        system.startTimeSecs :
          monasca.storm.system.startTimeSecs
        system.GC_ConcurrentMarkSweep.timeMs :
          monasca.storm.system.GC_ConcurrentMarkSweep.timeMs
    ```
    
    host: IP or host where the Monasca Agent running a StatsD is running that will consume
          the metrics produced by Storm / Threshold Engine
    
    port: UDP port number where the Monasca Agent running a StatsD daemon that will consume
          the metrics produced by Storm / Threshold Engine
    
    prefix: This string will be prefixed to the internal metric
    
    dimensions: A map of key/value pairs that will be passed along as dimensions for each metric
    
    whitelist: A list of metrics in the native name that Storm presents
    
    metricmap: A mapping from the native Storm metric name to a user defined name.  The user
               defined name is what will appear in the Monasca data store.  If there is no
               mapping present and it is listed in the whitelist then it will be published
               with the native name. The 12 metrics whitelisted/mapped above correspond to the
               monasca health dashboard we that is predefined in grafana.
               github.com/hpcloud-mon/grafana/src/app/dashboards/monasca.json
    
    Change-Id: I7bcefd03d02714ac42efd9b2d9cadb77907fa17e

--- thresh/src/main/java/monasca/thresh/utils/StatsdConfig.java
+++ thresh/src/main/java/monasca/thresh/utils/StatsdConfig.java
@@ -19,6 +19,7 @@ package monasca.thresh.utils;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.List;
 
 /*
  * Intended to deserialize the statsdConfig element in the
@@ -29,11 +30,10 @@ public class StatsdConfig implements Serializable {
   private static final long serialVersionUID = 3634080153227179376L;
 
   private String host;
-
   private Integer port;
-
-  private String prefix;
-
+  private List<String> whitelist;
+  private Boolean debugmetrics;
+  private Map<String, String> metricmap;
   private Map<String, String> dimensions;
 
   public Map<String, String> getDimensions() {
@@ -42,6 +42,30 @@ public class StatsdConfig implements Serializable {
 
   public void setDimensions(Map<String, String> dimensions) {
     this.dimensions = dimensions;
+  }
+
+  public List<String> getWhitelist() {
+    return whitelist;
+  }
+
+  public void setWhitelist(List<String> whitelist) {
+    this.whitelist = whitelist;
+  }
+
+  public Boolean getDebugmetrics() {
+    return debugmetrics;
+  }
+
+  public void setDebugmetrics(Boolean debugmetrics) {
+    this.debugmetrics = debugmetrics;
+  }
+
+  public Map<String, String> getMetricmap() {
+    return metricmap;
+  }
+
+  public void setMetricmap(Map<String, String> metricmap) {
+    this.metricmap = metricmap;
   }
 
   public String getHost() {
@@ -60,11 +84,5 @@ public class StatsdConfig implements Serializable {
     this.port = port;
   }
 
-  public String getPrefix() {
-    return prefix;
-  }
-
-  public void setPrefix(String prefix) {
-    this.prefix = prefix;
-  }
 }
+
