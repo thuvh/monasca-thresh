72fdac2d732db2ad63a63db95d74153addfc1f7e
Dexter Fryar <dexter.fryar@outlook.com>
2015-07-27 18:58:29 -0500

    Added a whitelist for restricting the StatsD metrics
    A whitelist and metric map for the metrics that are
    sent by Storm / Threshold Engine to the Monasca
    StatsD agent/daemon.
    
    Also relates to:
      https://github.com/hpcloud-mon/ansible-monasca-thresh/pull/14
    
    =======
    
    /etc/monasca/thresh-config.yml
    
    ```
    statsdConfig:
      host: localhost
      port: 8125
      debugmetrics: true
      dimensions: !!map
        service : monitoring
        component : storm
      whitelist: !!seq
        - aggregation-bolt.execute-count.filtering-bolt_alarm-creation-stream
        - aggregation-bolt.execute-count.filtering-bolt_default
        - aggregation-bolt.execute-count.system_tick
        - filtering-bolt.execute-count.event-bolt_metric-alarm-events
        - filtering-bolt.execute-count.metrics-spout_default
        - thresholding-bolt.execute-count.aggregation-bolt_default
        - thresholding-bolt.execute-count.event-bolt_alarm-definition-events
        - system.memory_heap.committedBytes
        - system.memory_nonHeap.committedBytes
        - system.newWorkerEvent
        - system.startTimeSecs
        - system.GC_ConcurrentMarkSweep.timeMs
      metricmap: !!map
        aggregation-bolt.execute-count.filtering-bolt_alarm-creation-stream :
          monasca.storm.aggregation-bolt.execute-count.filtering-bolt_alarm-creation-stream
        aggregation-bolt.execute-count.filtering-bolt_default :
          monasca.storm.aggregation-bolt.execute-count.filtering-bolt_default
        aggregation-bolt.execute-count.system_tick :
          monasca.storm.aggregation-bolt.execute-count.system_tick
        filtering-bolt.execute-count.event-bolt_metric-alarm-events :
          monasca.storm.filtering-bolt.execute-count.event-bolt_metric-alarm-events
        filtering-bolt.execute-count.metrics-spout_default :
          monasca.storm.filtering-bolt.execute-count.metrics-spout_default
        thresholding-bolt.execute-count.aggregation-bolt_default :
          monasca.storm.thresholding-bolt.execute-count.aggregation-bolt_default
        thresholding-bolt.execute-count.event-bolt_alarm-definition-events :
          monasca.storm.thresholding-bolt.execute-count.event-bolt_alarm-definition-events
        system.memory_heap.committedBytes :
          monasca.storm.system.memory_heap.committedBytes
        system.memory_nonHeap.committedBytes :
          monasca.storm.system.memory_nonHeap.committedBytes
        system.newWorkerEvent :
          monasca.storm.system.newWorkerEvent
        system.startTimeSecs :
          monasca.storm.system.startTimeSecs
        system.GC_ConcurrentMarkSweep.timeMs :
          monasca.storm.system.GC_ConcurrentMarkSweep.timeMs
    ```
    
    host: IP or host where the Monasca Agent running a StatsD is running that will consume
          the metrics produced by Storm / Threshold Engine
    
    port: UDP port number where the Monasca Agent running a StatsD daemon that will consume
          the metrics produced by Storm / Threshold Engine
    
    dimensions: A map of key/value pairs that will be passed along as dimensions for each metric
    
    whitelist: A list of metrics in the native name that Storm presents
    
    metricmap: A mapping from the native Storm metric name to a user defined name.  The user
               defined name is what will appear in the Monasca data store.  If there is no
               mapping present and it is listed in the whitelist then it will be published
               with the native name. The 12 metrics whitelisted/mapped above correspond to the
               monasca health dashboard which is defined in grafana.
    
               https://github.com/hpcloud-mon/grafana/blob/master/src/app/dashboards/monasca.json
    
    Change-Id: I7bcefd03d02714ac42efd9b2d9cadb77907fa17e

--- thresh/src/main/java/monasca/thresh/utils/StatsdMetricConsumer.java
+++ thresh/src/main/java/monasca/thresh/utils/StatsdMetricConsumer.java
@@ -19,8 +19,13 @@ package monasca.thresh.utils;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.lang.Boolean;
+import java.lang.String;
 import java.nio.charset.Charset;
+import java.util.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -47,14 +52,20 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
 
   public static final String STATSD_HOST = "metrics.statsd.host";
   public static final String STATSD_PORT = "metrics.statsd.port";
-  public static final String STATSD_PREFIX = "metrics.statsd.prefix";
+  public static final String STATSD_METRICMAP = "metrics.statsd.metricmap";
+  public static final String STATSD_WHITELIST = "metrics.statsd.whitelist";
   public static final String STATSD_DIMENSIONS = "metrics.statsd.dimensions";
+  public static final String STATSD_DEBUGMETRICS = "metrics.statsd.debugmetrics";
 
   String topologyName;
   String statsdHost = "localhost";
   int statsdPort = 8125;
-  String statsdPrefix = "monasca.storm.";
   String monascaStatsdDimPrefix = "|#";
+  List<String> whiteList = new ArrayList<String>();
+  Map<String, String> metricMap = new HashMap<String, String>();
+  Boolean debugMetrics = false;
+
+
   String defaultDimensions = new StringBuilder().append(monascaStatsdDimPrefix)
       .append("{\"service\":\"monitoring\",\"component\":\"storm\"}")
       .toString();
@@ -102,8 +113,7 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
     initClient();
 
     logger.info(
-        "statsdPrefix ({}), topologyName ({}), clean(topologyName) ({})",
-        new Object[] { statsdPrefix, topologyName, clean(topologyName) });
+        "topologyName ({}), clean(topologyName) ({})", new Object[] { topologyName, clean(topologyName) });
   }
 
   private void initClient() {
@@ -144,13 +154,6 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
       statsdPort = ((Number) conf.get(STATSD_PORT)).intValue();
     }
 
-    if (conf.containsKey(STATSD_PREFIX)) {
-      statsdPrefix = (String) conf.get(STATSD_PREFIX);
-      if (!statsdPrefix.endsWith(".")) {
-        statsdPrefix += ".";
-      }
-    }
-
     if (conf.containsKey(STATSD_DIMENSIONS)) {
       statsdDimensions = mapToJsonStr((Map<String, String>) conf
           .get(STATSD_DIMENSIONS));
@@ -163,6 +166,18 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
       else {
         statsdDimensions = monascaStatsdDimPrefix + statsdDimensions;
       }
+    }
+
+    if (conf.containsKey(STATSD_WHITELIST)) {
+      whiteList = (List) conf.get(STATSD_WHITELIST);
+    }
+
+    if (conf.containsKey(STATSD_METRICMAP)) {
+      metricMap = (Map) conf.get(STATSD_METRICMAP);
+    }
+
+    if (conf.containsKey(STATSD_DEBUGMETRICS)) {
+      debugMetrics = (Boolean) conf.get(STATSD_DEBUGMETRICS);
     }
   }
 
@@ -216,19 +231,17 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
   public void handleDataPoints(TaskInfo taskInfo,
       Collection<DataPoint> dataPoints) {
     for (Metric metric : dataPointsToMetrics(taskInfo, dataPoints)) {
-      report(metric.name, metric.value, metric.dimensions);
+      reportUOM(metric.name, metric.value);
     }
   }
 
   public static class Metric {
     String name;
     Double value;
-    String dimensions;
 
-    public Metric(String name, Double value, String dimensions) {
+    public Metric(String name, Double value) {
       this.name = name;
       this.value = value;
-      this.dimensions = dimensions;
     }
 
     @Override
@@ -248,15 +261,12 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
         return false;
       if (value != other.value)
         return false;
-      if (!dimensions.equals(other.dimensions))
-        return false;
       return true;
     }
 
     @Override
     public String toString() {
-      return "Metric [name=" + name + ", value=" + value + ", dimensions="
-          + dimensions + "]";
+      return "Metric [name=" + name + ", value=" + value + "]";
     }
   }
 
@@ -278,8 +288,7 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
           new Object[] { p.name, p.value });
 
       if (p.value instanceof Number) {
-        res.add(new Metric(sb.toString(), ((Number) p.value).doubleValue(),
-            statsdDimensions));
+        res.add(new Metric(sb.toString(), ((Number) p.value).doubleValue()));
       }
       // There is a map of data points and it's not empty
       else if (p.value instanceof Map && !(((Map<?, ?>) (p.value)).isEmpty())) {
@@ -292,8 +301,7 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
             sb.delete(hdrAndNameLength, sb.length());
             sb.append(".").append(clean(subName.toString()));
 
-            res.add(new Metric(sb.toString(),
-                ((Number) subValue).doubleValue(), statsdDimensions));
+            res.add(new Metric(sb.toString(), ((Number) subValue).doubleValue()));
           }
         }
       }
@@ -305,17 +313,50 @@ public class StatsdMetricConsumer implements IMetricsConsumer {
    * Since the Java client doesn't support the Monasca metric type we need to
    * build it with a raw UDP request
    */
-  public void report(String s, Double number, String dimensions) {
+  public void report(String s) {
     if (udpclient != null) {
-      StringBuilder statsdMessage = new StringBuilder().append(statsdPrefix)
-          .append(s).append(":").append(String.valueOf(number)).append("|c")
-          .append(statsdDimensions);
-      logger.debug("reporting: {}={}{}", s, number, dimensions);
-      udpclient.send(statsdMessage.toString());
+      logger.debug("reporting: {}", s);
+      udpclient.send(s);
     }
     else {
       /* Try to setup the UDP client since it was null */
       initClient();
+    }
+  }
+
+  private void reportUOM(String s, Double number) {
+    String metricName = null;
+    StringBuilder results = new StringBuilder();
+
+    if (whiteList.contains(s)) {
+      if (!metricMap.isEmpty() && metricMap.containsKey(s)) {
+        metricName = metricMap.get(s);
+      }
+      /* Send the unmapped uom as the same name storm calls it */
+      else {
+        metricName = s;
+      }
+    }
+
+    if (debugMetrics) {
+      String mappedName = new String();
+
+      if (!metricMap.isEmpty() && metricMap.containsKey(s)) {
+        mappedName = metricMap.get(s);
+      }
+      else {
+        mappedName = s;
+      }
+
+      logger.info(", RawMetricName, {}, MappedMetricName, {}, val, {}", new Object[]
+          { s, mappedName, number});
+    }
+
+    if (metricName != null && !metricName.isEmpty()) {
+      results = results.append(metricName).append(":")
+          .append(String.valueOf(number)).append("|c").append(statsdDimensions);
+
+      report(results.toString());
     }
   }
 
