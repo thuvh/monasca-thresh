aa8043918bdce171afe3e489380f4464350c4ced
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2016-03-15 09:12:39 +0100

    Perodic metric processing
    With new field 'period' for
    each metric, thresh component should
    be able to properly recognize it and
    act according to its value. Sparse metrics will
    cause associated alarms to never enter
    'UNDETERMINED' state, those with period will evaluate
    according to it and those with period not set
    will fallback to default behaviour.
    
    Implements: blueprint alarmonlogs
    Depends-On: Ia42f9a1be37c31416bdac341b092fe527f860c16
    Change-Id: Ibe0839123a15494ad45b809e68600c0acef3d330

--- thresh/src/main/java/monasca/thresh/domain/model/SubAlarmStats.java
+++ thresh/src/main/java/monasca/thresh/domain/model/SubAlarmStats.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * Copyright 2016 FUJITSU LIMITED
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -133,7 +134,7 @@ public class SubAlarmStats {
     }
     if (shouldSendStateChange(newState) &&
         (stats.shouldEvaluate(now, alarmDelay) ||
-         (newState == AlarmState.ALARM && this.subAlarm.canEvaluateImmediately()))) {
+            (newState == AlarmState.ALARM && this.subAlarm.canEvaluateImmediately()))) {
       setSubAlarmState(newState);
       return true;
     }
