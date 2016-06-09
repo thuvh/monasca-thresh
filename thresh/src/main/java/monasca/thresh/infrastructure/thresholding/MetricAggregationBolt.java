50d67b214e960642147f51b53fcbaf8fa76949d6
Craig Bryant <craig.bryant@hp.com>
2016-06-09 14:35:01 -0600

    Clone the currentValues property in duplicate method
    It is possible for the currentValues property to change which can cause
    java.util.ConcurrentModificationException. Fix by cloning currentValues
    before the SubAlarm gets emitted into storm
    
    Change-Id: I555beffafe0208c0d256732517af401938876d3d

--- thresh/src/main/java/monasca/thresh/infrastructure/thresholding/MetricAggregationBolt.java
+++ thresh/src/main/java/monasca/thresh/infrastructure/thresholding/MetricAggregationBolt.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * Copyright (c) 2014,2016 Hewlett-Packard Development Company, L.P.
  * Copyright 2016 FUJITSU LIMITED
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -42,9 +42,11 @@ import monasca.thresh.domain.service.SubAlarmStatsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -236,8 +238,16 @@ public class MetricAggregationBolt extends BaseRichBolt {
         new SubAlarm(original.getId(), original.getAlarmId(), new SubExpression(
             original.getAlarmSubExpressionId(), original.getExpression()), original.getState());
     newSubAlarm.setNoState(original.isNoState());
-    newSubAlarm.setCurrentValues(original.getCurrentValues());
+    cloneCurrentValues(original, newSubAlarm);
     return newSubAlarm;
+  }
+
+  private void cloneCurrentValues(final SubAlarm original, final SubAlarm newSubAlarm) {
+    final List<Double> originalCurrentValues = original.getCurrentValues();
+    if (originalCurrentValues == null) {
+      return;
+    }
+    newSubAlarm.setCurrentValues(new ArrayList<>(originalCurrentValues));
   }
 
   /**
