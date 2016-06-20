5dc075d9a0213a8ca4d862a735ade2382374e024
Craig Bryant <craig.bryant@hp.com>
2016-06-20 16:33:15 -0600

    Save Measurements that arrive before their SubAlarms
    Thresh creates an Alarm when a new Measurement matches an
    AlarmDefinition. The previous Thresh code just discarded the
    Measurement if it arrived before the newly created SubAlarm,
    which was likely to occur. This code saves a Measurement that
    does not match an existing SubAlarm in the expectation that the
    SubAlarm will arrive very soon. It then adds the Measurement
    to the SubAlarm.
    
    This is more important for determinstic alarms because they will get
    fewer Measurements and ignoring the first one may prevent an Alarm's
    state going to ALARM when it should
    
    Change-Id: I08e9e481ad55862ba602eba5a68eb371b1d35bbc

--- thresh/src/test/java/monasca/thresh/infrastructure/thresholding/MetricAggregationBoltTest.java
+++ thresh/src/test/java/monasca/thresh/infrastructure/thresholding/MetricAggregationBoltTest.java
@@ -394,7 +394,27 @@ public class MetricAggregationBoltTest {
     verify(collector, times(1)).emit(new Values(subAlarm2.getAlarmId(), subAlarm2));
   }
 
-  public void shouldNeverLeaveOkIfThresholdNotExceededForNonDeterministic() {
+  public void shouldSendAlarmIfMetricBeforeSubAlarm() {
+    long t1 = 50000;
+    bolt.execute(createMetricTuple(metricDef4, new Metric(metricDef4, t1, 1.0, null)));
+    t1 += 1000;
+    bolt.setCurrentTime(t1);
+    sendSubAlarmCreated(metricDef4, subAlarm4);
+    bolt.execute(createMetricTuple(metricDef4, new Metric(metricDef4, t1, 1.0, null)));
+    t1 += 1000;
+    bolt.execute(createMetricTuple(metricDef4, new Metric(metricDef4, t1, 1.0, null)));
+    t1 += 1000;
+    bolt.execute(createMetricTuple(metricDef4, new Metric(metricDef4, t1, 1.0, null)));
+    t1 += 1000;
+    bolt.execute(createMetricTuple(metricDef4, new Metric(metricDef4, t1, 1.0, null)));
+
+    bolt.setCurrentTime(t1 += 60000);
+    sendTickTuple();
+    assertEquals(subAlarm4.getState(), AlarmState.ALARM);
+    verify(collector, times(1)).emit(new Values(subAlarm4.getAlarmId(), subAlarm4));
+  }
+
+  public void shouldNeverLeaveOkIfThresholdNotExceededForDeterministic() {
     long t1 = 50000;
     bolt.setCurrentTime(t1);
     sendSubAlarmCreated(metricDef4, subAlarm4);
