f8872789c31567a542cd0d1b135652051e496abc
Michael James Hoppal <michael.jam.hoppal@hp.com>
2016-07-11 13:28:31 -0600

    Add millisecond resolution to alarms.
    At the moment it we are using the mysql built in function
    NOW() which returns in second resolution.
    
    Change-Id: I1192abb5aab3a9110721cc68f5a1d16a38f77c10

--- thresh/src/test/java/monasca/thresh/infrastructure/thresholding/AlarmThresholdingBoltTest.java
+++ thresh/src/test/java/monasca/thresh/infrastructure/thresholding/AlarmThresholdingBoltTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2014,2016 Hewlett Packard Enterprise Development Company LP.
+ * (C) Copyright 2014-2016 Hewlett Packard Enterprise Development LP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -128,7 +128,7 @@ public class AlarmThresholdingBoltTest {
             + "\"timestamp\":1395587091003}}";
 
     verify(alarmEventForwarder, times(1)).send(alarmJson);
-    verify(alarmDAO, times(1)).updateState(alarmId, AlarmState.ALARM);
+    verify(alarmDAO, times(1)).updateState(alarmId, AlarmState.ALARM, System.currentTimeMillis());
 
     // Now clear the alarm and ensure another notification gets sent out
     subAlarm.setState(AlarmState.OK);
@@ -153,7 +153,7 @@ public class AlarmThresholdingBoltTest {
             + "\"subAlarms\":[" + buildSubAlarmJson(alarm.getSubAlarms()) + "],"
             + "\"timestamp\":1395587091003}}";
     verify(alarmEventForwarder, times(1)).send(okJson);
-    verify(alarmDAO, times(1)).updateState(alarmId, AlarmState.OK);
+    verify(alarmDAO, times(1)).updateState(alarmId, AlarmState.OK, System.currentTimeMillis());
   }
 
   public void simpleAlarmUpdate() {
