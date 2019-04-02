6545a95b410383d7289662caa64e4dbcbd5ac15e
Michał Piotrowski <michal.piotrowski@ts.fujitsu.com>
2019-04-02 14:25:12 +0200

    Create Docker image and build in Zuul
    Change-Id: If3323a18ac3697a92ca3f06b0db154517582d7aa
    Story: 2001694
    Task: 30289

---
+++ docker/health_check.py
@@ -0,0 +1,10 @@
+"""Health check will returns 0 when service is working properly."""
+
+
+def main():
+    """No health check for monasca/thresh"""
+    return 0
+
+
+if __name__ == '__main__':
+    main()
